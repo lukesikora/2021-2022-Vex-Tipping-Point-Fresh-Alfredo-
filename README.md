4b76ba7b02a5c475055e96ba7a39c3d12aed1fbd
CodeMaster007 <75507820+CodeMasterBond@users.noreply.github.com>
2021-12-08 22:43:33 +0000

    Fixed

---
+++ README.md
@@ -0,0 +1,2 @@
+SHS 2025 VEX BASE
+#forked from ``https://github.com/Unionjackjz1/HW-AMOGO-VEXCODE``
--- testing.cpp
+++ testing.cpp
@@ -3,9 +3,5 @@ myVariable = 0
 def when_started1():
     global myVariable
     drivetrain.set_drive_velocity(10000, RPM)
-
-# define variable for remote controller enable/disable
-remote_control_code_enabled = True
-rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
-
-when_started1()
+    remote_control_code_enabled = True
+    rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
