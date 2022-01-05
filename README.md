f381ef4117e3c3aad50edf83e22888cb5d1e2e65
CodeMaster007 <75507820+CodeMasterBond@users.noreply.github.com>
2022-01-05 23:19:38 +0000

    updated rm and modified controls

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ SHS Freshman VEX TEAM/VEX-CODE
 
 #forked from ``https://github.com/Unionjackjz1/HW-AMOGO-VEXCODE``
 
-Last edited: 1/2/2022 1:40 PM
+Last edited: 1/5/2022 5:18 PM
---
+++ test123.cpp
@@ -0,0 +1,25 @@
+myVariable = 0
+
+def when_started1():
+    global myVariable
+    motor_15.set_velocity(70, PERCENT)
+
+def onevent_controller_1axis3Changed_0():
+    global myVariable
+    motor_15.spin(FORWARD)
+
+def onevent_controller_1buttonB_pressed_0():
+    global myVariable
+    motor_15.stop()
+
+# define variable for remote controller enable/disable
+remote_control_code_enabled = True
+rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
+
+# system event handlers
+controller_1.axis3.changed(onevent_controller_1axis3Changed_0)
+controller_1.buttonB.pressed(onevent_controller_1buttonB_pressed_0)
+# add 15ms delay to make sure events are registered correctly.
+wait(15, MSEC)
+
+when_started1()
