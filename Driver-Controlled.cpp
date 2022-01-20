93a90249758e7f1e4ab308bdeede17e9e8bbd5d9
CodeMaster007 <75507820+CodeMasterBond@users.noreply.github.com>
2022-01-20 23:13:22 +0000

    Changed controls for motor 15 (axis to button)

--- Driver-Controlled.cpp
+++ Driver-Controlled.cpp
@@ -16,9 +16,10 @@ def onevent_controller_1buttonB_pressed_0():
 remote_control_code_enabled = True
 rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
 
+
 # system event handlers
-controller_1.axis3.changed(onevent_controller_1axis3Changed_0)
 controller_1.buttonB.pressed(onevent_controller_1buttonB_pressed_0)
+controller_1.buttonY.pressed(onevent_controller_1buttonY_pressed_0)
 # add 15ms delay to make sure events are registered correctly.
 wait(15, MSEC)
 
