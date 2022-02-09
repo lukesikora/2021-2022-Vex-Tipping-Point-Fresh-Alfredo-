90fc9b5c0e988529e66a3cf78a4675d8a78bdae5
CodeMaster007 <75507820+CodeMasterBond@users.noreply.github.com>
2022-02-09 23:11:13 +0000

    Removed useless files, organized files with folders, fixed auto code

--- testing.cpp
+++
@@ -1,7 +0,0 @@
-myVariable = 0
-
-def when_started1():
-    global myVariable
-    drivetrain.set_drive_velocity(10000, RPM)
-    remote_control_code_enabled = True
-    rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
