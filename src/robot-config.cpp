035d0bd7bedc040672a23f22d2e374ebbc7bae0f
CodeMaster007 <75507820+CodeMasterBond@users.noreply.github.com>
2022-02-23 21:51:20 +0000

    Updated Robot config file w updated wire setup

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -9,16 +9,18 @@ brain  Brain;
 
 // VEXcode device constructors
 controller Controller1 = controller(primary);
-motor leftMotorA = motor(PORT4, ratio36_1, true);
-motor leftMotorB = motor(PORT6, ratio36_1, true);
+motor leftMotorA = motor(PORT6, ratio36_1, true);
+motor leftMotorB = motor(PORT17, ratio36_1, true);
 motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB);
-motor rightMotorA = motor(PORT5, ratio36_1, false);
-motor rightMotorB = motor(PORT17, ratio36_1, false);
+motor rightMotorA = motor(PORT4, ratio36_1, false);
+motor rightMotorB = motor(PORT5, ratio36_1, false);
 motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB);
 drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
 motor Motor18 = motor(PORT18, ratio18_1, false);
 motor Motor15 = motor(PORT15, ratio6_1, false);
-motor Motor14 = motor(PORT14, ratio36_1, false);
+motor Motor14 = motor(PORT14, ratio36_1, true);
+motor Motor19 = motor(PORT19, ratio36_1, false);
+motor Motor12 = motor(PORT12, ratio36_1, true);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
