8a52e51fcc7f7d7697d90ea302838829ab330728
CodeMaster007 <75507820+CodeMasterBond@users.noreply.github.com>
2022-02-23 21:50:51 +0000

    Updated main.cpp and removed conf

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,12 @@
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
 // Controller1          controller                    
-// Drivetrain           drivetrain    4, 6, 5, 17     
+// Drivetrain           drivetrain    6, 17, 4, 5     
 // Motor18              motor         18              
 // Motor15              motor         15              
 // Motor14              motor         14              
+// Motor19              motor         19              
+// Motor12              motor         12              
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -23,7 +25,6 @@ competition Competition;
 float myVariable;
 
 //**********************************************************************************************
-// "when Controller1 ButtonX pressed" hat block
 void onevent_Controller1ButtonX_pressed_0() {
   Motor15.setVelocity(100.0, percent);
   Motor15.spin(forward);
@@ -31,55 +32,45 @@ void onevent_Controller1ButtonX_pressed_0() {
   Motor15.stop();
   Motor15.setVelocity(70.0, percent);
 }
-// "when Controller1 ButtonA pressed" hat block
 void onevent_Controller1ButtonA_pressed_0() {
+  Motor14.setVelocity(100.0, percent);
+  Motor14.spinFor(reverse, 300, degrees, true);
+}
+// "when Controller1 ButtonA pressed" hat block
+
+void onevent_Controller1ButtonL1_pressed_0() {
   Motor14.setMaxTorque(100, percent); 
   Motor14.setVelocity(80, percent);
   Motor14.spin(forward);
 }
 
-// "when Controller1 ButtonA released" hat block
-void onevent_Controller1ButtonA_released_0() {
+void onevent_Controller1ButtonL1_released_0() {
   Motor14.stop();
 }
 
 // "when Controller1 ButtonB pressed" hat block
-void onevent_Controller1ButtonB_pressed_0() { //   UP
+void onevent_Controller1ButtonL2_pressed_0() { //   UP
   Motor14.setVelocity(80, percent);
   Motor14.setMaxTorque(100, percent); 
-  Motor14.spinFor(reverse, 300.0, degrees, true);
+  Motor14.spin(reverse);
+  //Motor14.spinFor(reverse, 300.0, degrees, true);
+  //wait(0.5, seconds);
+  //Motor14.spinFor(reverse, 250.0, degrees, true);
 }
-
-// "when Controller1 ButtonB released" hat block
-void onevent_Controller1ButtonB_released_0() {
+void onevent_Controller1ButtonL2_released_0() {
   Motor14.stop();
 }
-// "when Controller1 ButtonR1 released" hat block
-void onevent_Controller1ButtonR1_released_0() {
-  Motor18.stop();
-}
-
-// "when Controller1 ButtonR1 pressed" hat block
-void onevent_Controller1ButtonR1_pressed_0() {
-  Motor18.spin(reverse);
-}
-
-// "when Controller1 ButtonR2 pressed" hat block
-void onevent_Controller1ButtonR2_pressed_0() {
-  Motor18.spin(forward);
-}
-
 // "when Controller1 ButtonL2 pressed" hat block
-void onevent_Controller1ButtonL2_pressed_0() {
-  Motor15.spin(reverse);
-}
-
-// "when Controller1 ButtonL1 pressed" hat block
-void onevent_Controller1ButtonL1_pressed_0() {
+void onevent_Controller1ButtonR1_pressed_0() {
   Motor15.stop();
 }
 
-// "when Controller1 ButtonA pressed" hat block
+// "when Controller1 ButtonL1 pressed" hat block
+void onevent_Controller1ButtonR2_pressed_0() {
+  Motor15.spin(reverse);
+}
+
+
 
 //**********************************************************************************************
 //SMILEYFACE
@@ -90,22 +81,56 @@ void drawSmileyAt(int x, int y){
   Brain.Screen.drawRectangle(x-20, y+10, 40, 10, red);
 }
 //AUTONOMOUS
+//Motor14 - Pickup
+//Motor15 - Convyeror
 int onauton_autonomous_0() {
   Motor15.setVelocity(90.0, percent);
-  Motor15.setVelocity(75.0, percent);
-  Motor18.spin(reverse);
-  wait(1.0, seconds);
+  Motor14.setVelocity(75.0, percent);
   Drivetrain.driveFor(forward, 200.0, mm, true);
   Drivetrain.stop();
-  Motor18.spin(forward);
+  Motor14.spin(reverse);
   Motor15.spin(reverse);
   wait(4.0, seconds);
   Motor15.stop();
   Drivetrain.driveFor(reverse, 200.0, mm, true);
-  Motor18.spinFor(reverse, 270.0, degrees, true);
-  Motor18.stop();
+  Motor14.spinFor(forward, 270.0, degrees, true);
+  Motor14.stop();
   return 0;
 }
+    /*
+  
+  Motor14.spinFor(reverse, 260.0, degrees, true);
+  Motor15.spin(reverse);
+  wait(4.0, seconds);
+  Motor15.stop();
+  Drivetrain.driveFor(reverse, 330.0, mm, true);
+  Motor14.stop();
+  return 0; 
+}
+
+
+int onauton_autonomous_0() {
+  Motor14.setVelocity(75.0, percent);
+  Motor15.setVelocity(100.0, percent);
+  Drivetrain.setDriveVelocity(40, percent); 
+  Motor14.spinFor(forward, 330.0, degrees, true);
+  wait(1, seconds);
+  Drivetrain.driveFor(forward, 150.0, mm, true);
+  Drivetrain.stop();
+  wait(1, seconds);
+  Drivetrain.driveFor(forward, 10.0, mm, true);
+  Drivetrain.stop();
+  Motor14.spinFor(reverse, 300.0, degrees, true);
+  wait(1, seconds);
+  Motor14.spinFor(reverse, 260.0, degrees, true);
+  Motor15.spin(reverse);
+  wait(4.0, seconds);
+  Motor15.stop();
+  Drivetrain.driveFor(reverse, 180.0, mm, true);
+  Motor14.stop();
+  return 0;
+}
+*/
 //AUTONOMOUS SETUP
 void VEXcode_auton_task() {
   // Start the auton control tasks....
@@ -148,15 +173,15 @@ int main() {
   Competition.drivercontrol(VEXcode_driver_task);
   vexcodeInit();
   // register event handlers
-  Controller1.ButtonX.pressed(onevent_Controller1ButtonX_pressed_0);
-  Controller1.ButtonR1.released(onevent_Controller1ButtonR1_released_0);
+  
+  //Controller1.ButtonR1.released(onevent_Controller1ButtonR1_released_0);
   Controller1.ButtonR1.pressed(onevent_Controller1ButtonR1_pressed_0);
   Controller1.ButtonR2.pressed(onevent_Controller1ButtonR2_pressed_0);
   Controller1.ButtonL2.pressed(onevent_Controller1ButtonL2_pressed_0);
   Controller1.ButtonL1.pressed(onevent_Controller1ButtonL1_pressed_0);
+  Controller1.ButtonX.pressed(onevent_Controller1ButtonX_pressed_0);
   Controller1.ButtonA.pressed(onevent_Controller1ButtonA_pressed_0);
-  Controller1.ButtonB.pressed(onevent_Controller1ButtonB_pressed_0);
-  Controller1.ButtonA.released(onevent_Controller1ButtonA_released_0);
-  Controller1.ButtonB.released(onevent_Controller1ButtonB_released_0);
+  Controller1.ButtonL2.released(onevent_Controller1ButtonL2_released_0);
+  Controller1.ButtonL1.released(onevent_Controller1ButtonL1_released_0);
 }
 //**********************************************************************************************
